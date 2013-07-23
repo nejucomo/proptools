2445e295119895c737457b715b505bc74fc4593a
Nathan Wilcox <nejucomo@gmail.com>
2013-07-22 18:47:28 -0700

    Add TypedProperty.

--- proptools.py
+++ proptools.py
@@ -16,6 +16,31 @@ class LazyProperty (property):
             return value
 
 
+class TypedProperty (property):
+    def __init__(self, type):
+        self._values = WeakKeyDictionary()
+        self.type = type
+
+    def __get__(self, instance, _cls):
+        try:
+            return self._values[instance]
+        except KeyError:
+            raise AttributeError('%r object has no such attribute.' % (type(instance).__name__,))
+
+    def __set__(self, instance, value):
+        if isinstance(value, self.type):
+            self._values[instance] = value
+        else:
+            raise TypeError('Property values must be instances of %r; not %r' % (self.type, type(value)))
+
+    def __delete__(self, instance):
+        try:
+            del self._values[instance]
+        except KeyError:
+            raise AttributeError('%r object has no such attribute.' % (type(instance).__name__,))
+
+
+
 # unittests:
 class LazyPropertyTests (unittest.TestCase):
     def setUp(self):
@@ -37,6 +62,40 @@ class LazyPropertyTests (unittest.TestCase):
 
     def test_readonly(self):
         self.assertRaises(AttributeError, setattr, self.i, 'foo', 42)
+
+
+class TypedPropertyTests (unittest.TestCase):
+    def setUp(self):
+        class C (object):
+            i = TypedProperty(int)
+
+        self.C = C
+        self.obj = C()
+
+    def test_set_success(self):
+        self.obj.i = 42
+        self.assertEqual(42, self.obj.i)
+
+    def test_set_wrong_type(self):
+        self.assertRaises(TypeError, setattr, self.obj, 'i', 'banana')
+
+    def test_get_unset(self):
+        self.assertRaises(AttributeError, getattr, self.obj, 'i')
+
+    def test_set_then_del_then_getattr(self):
+        self.obj.i = 42
+        del self.obj.i
+        self.assertRaises(AttributeError, getattr, self.obj, 'i')
+
+    def test_del_twice(self):
+        self.obj.i = 42
+
+        def del_i():
+            del self.obj.i
+
+        del_i()
+
+        self.assertRaises(AttributeError, del_i)
 
 
 
