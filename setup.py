ed5dc5dd72dfc5f030e2d83508d7c42fc667baf1
Nathan Wilcox <nejucomo@gmail.com>
2013-07-22 17:38:33 -0700

    Added a ./setup.py for proptools.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import sys
+from distutils.core import setup
+
+
+if 'upload' in sys.argv:
+    if '--sign' not in sys.argv and sys.argv[1:] != ['upload', '--help']:
+        raise SystemExit('Refusing to upload unsigned packages.')
+
+
+setup(name = 'proptools',
+      description = 'Various useful property subtypes.',
+      url = 'https://github.org/nejucomo/proptools',
+      license = 'GPLv3',
+      version = '0.1a0',
+      author = 'Nathan Wilcox',
+      author_email = 'nejucomo@gmail.com',
+      py_modules = ['proptools'],
+      )
